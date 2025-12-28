3d11bc09a05d5757b1d946dc9eaa9abe597d16f8
anjali811saini <anjalisaini8273460553@gmail.com>
2025-12-28 12:40:56 +0530

    change

---
+++ git_Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            int sum = num1 + num2;
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
