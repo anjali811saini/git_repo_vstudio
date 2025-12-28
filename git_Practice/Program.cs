88e5c0c33d167939c94340d566fc11115e910a2a
anjali811saini <anjalisaini8273460553@gmail.com>
2025-12-28 13:07:03 +0530

    changes

--- git_Practice/Program.cs
+++ git_Practice/Program.cs
@@ -16,7 +16,7 @@ namespace git_Practice
             int sum = num1 + num2;
 
             Console.WriteLine(sum);
-
+            Console.Write("");
             Console.ReadKey();
         }
     }
